6aeba62b252e4eb70bfe551d0675e705ada12d3c
Jack Werner <jack.oliver.werner@gmail.com>
2017-02-17 16:12:48 -0600

    More analysis on Ervin

--- Ervin_Santana.R
+++ Ervin_Santana.R
@@ -93,8 +93,8 @@ ggplot(data = ervin, aes(pfx_x, pfx_z, color = simple_pitch_type)) + geom_point(
 # Velocity
 ggplot(data = ervin, aes(start_speed)) + facet_grid(simple_pitch_type~.) + geom_histogram()
 
-ggplot(data = ervin, aes(start_speed, fill = simple_pitch_type)) + 
-  geom_density(alpha = .5, color = "grey50", size = 1)
+ggplot(data = ervin, aes(start_speed, fill = simple_pitch_type, color = simple_pitch_type)) + 
+  geom_density(alpha = .5, size = 1)
 
 ervin <- ervin %>% 
   mutate(simple_pitch_type = ifelse(simple_pitch_type == "FT", "FF", simple_pitch_type))
@@ -105,11 +105,14 @@ ervin <- ervin %>%
 
 ervin.mat <- ervin %>% select(pfx_x, pfx_z, start_speed) %>% as.matrix() %>% scale()
 
-ervin.km <- kmeans(ervin.mat, centers = 3)
+ervin$cluster <- kmeans(ervin.mat, centers = 3)$cluster
 
-table(ervin.km$cluster, ervin$simple_pitch_type)
+(clust.tab <- table(ervin$cluster, ervin$simple_pitch_type))
 
-ervin$cluster_type <- ifelse(ervin.km$cluster == 1, "FF", ifelse(ervin.km$cluster == 2, "SL", "CH"))
+conv.df <- data.frame(cluster = as.numeric(as.character(rownames(clust.tab))), 
+                      cluster_type = colnames(clust.tab)[apply(clust.tab, 1, which.max)])
+
+ervin <- ervin %>% left_join(conv.df, by = "cluster")
 
 ervin <- ervin %>% mutate(mismatch = simple_pitch_type == cluster_type)
 
@@ -139,7 +142,20 @@ ggplot(data = ervin, aes(start_speed, pfx_z, color = mismatch)) + geom_point() +
   scale_color_manual(values = c("red", "grey70"))
 
 
-# Pitch selection by count and handedness
+###############################
+# Pitches by count/handedness #
+###############################
+
+tables <- ervin %>% group_by(count, balls, strikes, b_hand) %>%
+  summarize(FF = sum(simple_pitch_type == "FF"),
+            SL = sum(simple_pitch_type == "SL"),
+            CH = sum(simple_pitch_type == "CH"),
+            FF_p = FF/n(), SL_p = SL/n(), CH_p = CH/n(), total = n()) %>%
+  ungroup()
+
+tables %>% filter(b_hand == "R") %>% as.data.frame()
+tables %>% filter(b_hand == "L") %>% as.data.frame()
+
 ervin$simple_pitch_type <- factor(ervin$simple_pitch_type, levels = c("SL", "CH", "FF"))
 
 ggplot(data = ervin, aes(b_hand, fill = simple_pitch_type)) + facet_grid(strikes~balls) + geom_bar(position = "fill")
@@ -188,6 +204,9 @@ ggplot(data = filter(ervin, count == "0-2"), aes(px, pz, color = pitch_ab_res)) 
   coord_fixed() +
   scale_color_manual(values = c("grey40", "red", "blue", "purple"))
 
+(tab.02 <- table(ervin$simple_pitch_type[ervin$count == "0-2"], ervin$b_hand[ervin$count == "0-2"]))
+prop.table(tab.02, 2)
+
 # 1-2 count by type, result, hand
 ggplot(data = filter(ervin, count == "1-2"), aes(px, pz, color = pitch_ab_res)) + 
   facet_grid(b_hand~simple_pitch_type) +
@@ -195,6 +214,9 @@ ggplot(data = filter(ervin, count == "1-2"), aes(px, pz, color = pitch_ab_res)) 
   geom_polygon(data = strike.zone, aes(x = x, y = y, color = NA), fill = NA, color = "black") +
   coord_fixed() +
   scale_color_manual(values = c("grey40", "orange", "red", "blue", "purple"))
+
+(tab.12 <- table(ervin$simple_pitch_type[ervin$count == "1-2"], ervin$b_hand[ervin$count == "1-2"]))
+prop.table(tab.12, 2)
 
 # 2 strikes by count, hand, type
 ggplot(data = filter(ervin, strikes == 2), aes(px, pz, color = simple_pitch_type)) + 
@@ -224,25 +246,188 @@ table(ervin$count[ervin$last & ervin$simple_event == "K"],
 # Pitch sequencing #
 ####################
 
-
-
-ervin %>% filter(pitch_ab_res == "K", pz > 3.25) %>% arrange(desc(pz)) %>% View()
-
-
-###############################
-# Pitches by count/handedness #
-###############################
-
-tables <- ervin %>% group_by(count, balls, strikes, b_hand) %>%
-  summarize(FF = sum(simple_pitch_type == "FF"),
-            SL = sum(simple_pitch_type == "SL"),
-            CH = sum(simple_pitch_type == "CH"),
-            FF_p = FF/n(), SL_p = SL/n(), CH_p = CH/n(), total = n()) %>%
+ervin.seq <- ervin %>% group_by(gid, ab_num) %>%
+  mutate(prev_count = lag(count, 1, default = "None"),
+         prev_pitch = lag(as.character(simple_pitch_type), 1, default = "None"),
+         next_pitch = lead(as.character(simple_pitch_type), 1, default = "None"),
+         pitch_num = 1:n()) %>%
   ungroup()
 
-tables %>% filter(b_hand == "R") %>% as.data.frame()
-tables %>% filter(b_hand == "L") %>% as.data.frame()
+
+ervin.seq %>% select(pitch_result, prev_pitch, simple_pitch_type, next_pitch) %>% View()
 
 
+table(ervin.seq$simple_pitch_type, ervin.seq$prev_pitch) %>% prop.table(1)
+
+
+##### Individual Counts #####
+
+# 0-2 count
+ggplot(data = filter(ervin.seq, count == "0-2"), aes(px, pz, color = simple_pitch_type)) + 
+  facet_grid(prev_count~prev_pitch) +
+  geom_point(size = 2) +
+  geom_polygon(data = strike.zone, aes(x = x, y = y, color = NA), fill = NA, color = "black") +
+  coord_fixed()
+
+table(ervin.seq$prev_pitch[ervin$count == "0-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "0-2"]) %>%
+  prop.table(1)
+
+table(ervin.seq$prev_pitch[ervin$count == "0-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "0-2"],
+      ervin.seq$b_hand[ervin$count == "0-2"]) %>%
+  prop.table(c(1, 3))
+
+ggplot(data = filter(ervin.seq, count == "0-2"), aes(x = b_hand, fill = simple_pitch_type)) +
+  facet_grid(prev_count~prev_pitch) + geom_bar(position = "fill")
+
+
+# 1-2 count
+ggplot(data = filter(ervin.seq, count == "1-2"), aes(px, pz, color = simple_pitch_type)) + 
+  facet_grid(prev_count~prev_pitch) +
+  geom_point(size = 2) +
+  geom_polygon(data = strike.zone, aes(x = x, y = y, color = NA), fill = NA, color = "black") +
+  coord_fixed()
+
+table(ervin.seq$prev_pitch[ervin$count == "1-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "1-2"]) %>%
+  prop.table(1)
+
+table(ervin.seq$prev_pitch[ervin$count == "1-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "1-2"],
+      ervin.seq$b_hand[ervin$count == "1-2"]) %>%
+  prop.table(c(1, 3))
+
+ggplot(data = filter(ervin.seq, count == "1-2"), aes(x = b_hand, fill = simple_pitch_type)) +
+  facet_grid(prev_count~prev_pitch) + geom_bar(position = "fill")
+
+# He throws changeups after fouled off pitches
+table((ervin.seq$prev_count == "1-2")[ervin.seq$count == "1-2"],
+      (ervin.seq$simple_pitch_type == "CH")[ervin.seq$count == "1-2"]) %>%
+  prop.table(c(1))
+
+
+# 2-2 count
+ggplot(data = filter(ervin.seq, count == "2-2"), aes(px, pz, color = simple_pitch_type)) + 
+  facet_grid(prev_count~prev_pitch) +
+  geom_point(size = 2) +
+  geom_polygon(data = strike.zone, aes(x = x, y = y, color = NA), fill = NA, color = "black") +
+  coord_fixed()
+
+table(ervin.seq$prev_pitch[ervin$count == "2-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "1-2"]) %>%
+  prop.table(1)
+
+table(ervin.seq$prev_pitch[ervin$count == "2-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "2-2"],
+      ervin.seq$b_hand[ervin$count == "2-2"]) %>%
+  prop.table(c(1, 3))
+
+ggplot(data = filter(ervin.seq, count == "2-2"), aes(x = b_hand, fill = simple_pitch_type)) +
+  facet_grid(prev_count~prev_pitch) + geom_bar(position = "fill")
+
+
+# 3-2 count
+ggplot(data = filter(ervin.seq, count == "3-2"), aes(px, pz, color = simple_pitch_type)) + 
+  facet_grid(prev_count~prev_pitch) +
+  geom_point(size = 2) +
+  geom_polygon(data = strike.zone, aes(x = x, y = y, color = NA), fill = NA, color = "black") +
+  coord_fixed()
+
+table(ervin.seq$prev_pitch[ervin$count == "3-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "3-2"]) %>%
+  prop.table(1)
+
+table(ervin.seq$prev_pitch[ervin$count == "3-2"],
+      ervin.seq$simple_pitch_type[ervin$count == "3-2"],
+      ervin.seq$b_hand[ervin$count == "3-2"]) %>%
+  prop.table(c(1, 3))
+
+ggplot(data = filter(ervin.seq, count == "3-2"), aes(x = b_hand, fill = simple_pitch_type)) +
+  facet_grid(prev_count~prev_pitch) + geom_bar(position = "fill")
+
+
+
+# Second pitch
+ggplot(data = filter(ervin.seq, count %in% c("1-0", "0-1")), aes(x = b_hand, fill = simple_pitch_type)) +
+  facet_grid(count~prev_pitch) + geom_bar(position = "fill")
+
+
+
+#################
+# Pitch strings #
+#################
+
+# Actual
+
+abid <- paste0(ervin$gid, ervin$ab_num)
+pitches.1 <- ervin$simple_pitch_type %>% as.character() %>% substr(1, 1)
+
+seqs <- tapply(pitches.1, abid, paste0, collapse = "", simplify = T) %>%
+  as.vector()
+
+ps <- c("F", "S", "C")
+all.duos <- paste0(rep(ps, each = 3), ps)
+all.trios <- paste0(rep(all.duos, each = 3), ps)
+
+
+duos.to.match <- paste0("(?=", all.duos, ")")
+duos.freq <- rep(0, length(all.duos))
+
+for (i in 1:length(all.duos)) {
+  duos.freq[i] <- gregexpr(duos.to.match[i], seqs, perl = T) %>% sapply(function(x){sum(x>0)}) %>% sum()
+}
+
+
+trios.to.match <- paste0("(?=", all.trios, ")")
+trios.freq <- rep(0, length(all.trios))
+
+for (i in 1:length(all.trios)) {
+  trios.freq[i] <- gregexpr(trios.to.match[i], seqs, perl = T) %>% sapply(function(x){sum(x>0)}) %>% sum()
+}
+
+seqs.df <- data.frame(pattern = c(all.duos, all.trios), freq = c(duos.freq, trios.freq))
+
+
+# Random
+
+rand.freqs <- matrix(0, nrow = length(all.duos) + length(all.trios), ncol = 100)
+
+for (j in 1:100) {
+  pitches.1.r <- ervin$simple_pitch_type %>% as.character() %>% substr(1, 1) %>% sample()
+  
+  seqs.r <- tapply(pitches.1.r, abid, paste0, collapse = "", simplify = T) %>%
+    as.vector()
+  
+  duos.freq.r <- rep(0, length(all.duos))
+  
+  for (i in 1:length(all.duos)) {
+    duos.freq.r[i] <- gregexpr(duos.to.match[i], seqs.r, perl = T) %>% sapply(function(x){sum(x>0)}) %>% sum()
+  }
+  
+  
+  trios.freq.r <- rep(0, length(all.trios))
+  
+  for (i in 1:length(all.trios)) {
+    trios.freq.r[i] <- gregexpr(trios.to.match[i], seqs.r, perl = T) %>% sapply(function(x){sum(x>0)}) %>% sum()
+  }
+  
+  rand.freqs[,j] <- c(duos.freq.r, trios.freq.r)
+  
+  print(j)
+}
+
+seqs.df <- data.frame(pattern = c(all.duos, all.trios), 
+                      freq = c(duos.freq, trios.freq), 
+                      exp = apply(rand.freqs, 1, mean))
+
+
+ggplot(data = seqs.df[1:9,], aes(x = exp, y = freq, label = pattern)) + 
+  geom_text() + 
+  geom_abline(slope = 1, intercept = 0, color = "red")
+
+ggplot(data = seqs.df[10:nrow(seqs.df),], aes(x = exp, y = freq, label = pattern)) + 
+  geom_text() + 
+  geom_abline(slope = 1, intercept = 0, color = "red")
 
 
